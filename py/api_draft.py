c7f56c1f86018d2fd452361548b636257bc8a8a5
Nima Mousavi <nima.moosavi@gmail.com>
2022-12-28 15:10:56 -0800

    updated api draft

--- py/api_draft.py
+++ py/api_draft.py
@@ -1,5 +1,17 @@
 # used for inspiration: https://github.com/ericdwang/pybart
+import sys
+
+sys.path.append('/home/nmmsv/repos/pybart')
+from pybart.api import BART
 
 api_key = 'MW9S-E7SL-26DU-VV8V'
 BART_URL = 'https://api.bart.gov/api/{api}.aspx'
 
+
+bart = BART()
+
+root = bart.stn.stninfo('dbrk')
+station = root.find('stations').find('station')
+
+
+# https://api.bart.gov/api/etd.aspx?cmd=etd&orig=ROCK&key=MW9S-E7SL-26DU-VV8V

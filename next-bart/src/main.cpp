5f7f6caf2e858815b4c6fda9be16232019618c54
Nima Mousavi <nima.moosavi@gmail.com>
2022-12-29 13:36:13 -0800

    got LCD to work for the first time!

--- next-bart/src/main.cpp
+++ next-bart/src/main.cpp
@@ -2,9 +2,12 @@
 #include <WiFi.h>
 #include <HTTPClient.h>
 #include <Arduino_JSON.h>
+#include <LiquidCrystal.h>
 
 #define WIFI_SSID "NeemsWiFi2.4"
 #define WIFI_PASSWORD "HolaSoyDora420"
+// initialize the library with the numbers of the interface pins
+LiquidCrystal lcd(19, 23, 18, 17, 16, 15);
 
 std::vector<int> extract_etd_time(String payload, String direction);
 
@@ -25,10 +28,21 @@ void setup()
   Serial.println("");
   Serial.print("Connected. Local IP Address: ");
   Serial.println(WiFi.localIP());
+
+  // set up the LCD's number of columns and rows:
+  lcd.begin(16, 2);
+  // Print a message to the LCD.
+  lcd.print("circuitschools.");
 }
 
 void loop()
 {
+  // set the cursor to column 0, line 1
+  // (note: line 1 is the second row, since counting begins with 0):
+  lcd.setCursor(0, 1);
+  // print the number of seconds since reset:
+  lcd.print(millis() / 1000);
+
   // put your main code here, to run repeatedly:
   String api_path = "https://api.bart.gov/api/etd.aspx?cmd=etd&orig=ROCK&key=MW9S-E7SL-26DU-VV8V&json=y";
   HTTPClient http;
@@ -39,7 +53,9 @@ void loop()
   {
     String payload = http.getString();
 
+    // extract southbound estimated time of departure
     std::vector<int> etds = extract_etd_time(payload, "South");
+
     Serial.print("South: ");
     for (int k = 0; k < etds.size(); k++)
     {
